5d5854385c66e64b43f8087b601e2b19e43db1b1
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-22 00:13:48 +0700

    Update login_screen.dart

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -7,75 +7,96 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(25.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "Hello",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
-            ),
-            const Text(
-              "Sign in to your account",
-              style: TextStyle(color: Colors.black26),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Customtextfeild(
-              borderColor: Colors.black12,
-              hintText: "Username",
-              //labelText: "Username",
-            ),
-            const Customtextfeild(
-              borderColor: Colors.black12,
-              hintText: "Password",
-              obscureText: true,
-              //labelText: "Password",
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: const [
-                Text(
-                  "Forgot your password?",
-                  style: TextStyle(color: Colors.black26),
-                ),
-              ],
-            ),
-            const Padding(
-              padding: EdgeInsets.all(25.0),
-              child: CustomButton(
-                text: "SIGN IN",
-                fillColor: Colors.pink,
-                textStyle: TextStyle(
+    return Container(
+      constraints: const BoxConstraints.expand(),
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(
+              "https://images.unsplash.com/photo-1572191783453-62f99a6055ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Hello",
+                style: TextStyle(
                   fontWeight: FontWeight.bold,
-                  fontSize: 18,
+                  fontSize: 50,
                   color: Colors.white,
                 ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Don't have an account?"),
-                const SizedBox(
-                  width: 5,
-                ),
-                GestureDetector(
-                  onTap: () {},
-                  child: const Text(
-                    "Create",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
+              const Text(
+                "Sign in to your account",
+                style: TextStyle(color: Colors.white, fontSize: 15),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Customtextfeild(
+                borderColor: Colors.white,
+                hintText: "Username",
+                //labelText: "Username",
+              ),
+              const Customtextfeild(
+                borderColor: Colors.white,
+                hintText: "Password",
+                obscureText: true,
+                //labelText: "Password",
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: const [
+                  Text(
+                    "Forgot your password?",
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: CustomButton(
+                  text: "SIGN IN",
+                  fillColor: Colors.pink[400],
+                  textStyle: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Colors.white,
                   ),
                 ),
-              ],
-            ),
-          ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Don't have an account?",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: const Text(
+                      "Create",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
