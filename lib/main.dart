f542e2f377cbc04bf18824366e45a6fd823c405c
CHHAILENG TIM <80568234+ChhailengTim@users.noreply.github.com>
2022-06-24 14:48:58 +0700

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:register/screen/login_screen.dart';
+
+import 'payment/screen/payment_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: PaymentScreen(),
     );
   }
 }
