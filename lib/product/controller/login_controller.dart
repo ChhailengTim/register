e9cfa51f361acc3829b9c16c66af7780839dab47
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-07-06 13:30:12 +0700

    Create login_controller.dart

---
+++ lib/product/controller/login_controller.dart
@@ -0,0 +1,10 @@
+//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+
+class LoginController extends GetxController {
+  //final FirebaseAuth auth = FirebaseAuth.instance;
+
+  final email = ''.obs;
+  final password = ''.obs;
+  final confirmpass = ''.obs;
+}
