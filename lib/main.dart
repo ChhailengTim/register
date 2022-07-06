18f2b3667007426938e88c76fcc788d61b496bc7
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-07-06 13:29:50 +0700

    firebase fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:register/spotify_music_player/screen/player_screen.dart';
+
+import 'product/screen/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PlayerScreen(),
+      home: LoginScreenForm(),
     );
   }
 }
